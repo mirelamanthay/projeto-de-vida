ef6438d18201de9f3851ca08a4c8b1600452c54f
mirelamanthay <166140891+mirelamanthay@users.noreply.github.com>
2026-04-30 09:40:42 -0300

    s

--- main.js
+++ main.js
@@ -10,3 +10,27 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+
